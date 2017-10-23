f5be6d0f4ff4531e8f7f9f2f75d630d5687f906c
Mike Steel <mike@qubitdigital.com>
2017-10-23 16:46:20 +0100

    make it all stylish

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,13 +1,16 @@
 const fs = require('fs')
-const template = fs.readFileSync('src/public/index.html')
+const template = fs.readFileSync('src/public/html/index.html')
+const footer = fs.readFileSync('src/public/html/footer.html')
+const header = fs.readFileSync('src/public/html/header.html')
 
 module.exports = function () {
   this.render = config => {
     return template.toString()
       .replace(/{{jeffs}}/, config.data.map((jeff, i) => {
         return getJeffs(jeff)
-      }).join('')
-    )
+      }).join(''))
+      .replace(/{{footer}}/, footer)
+      .replace(/{{header}}/, header)
   }
 }
 
